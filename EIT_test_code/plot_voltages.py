0201551df04aa4dedf57af9dd93315b5c0ebee99
Charlie DeLorey <delorey.charlie@gmail.com>
2021-05-18 11:34:30 +0100

    work

--- EIT_test_code/plot_voltages.py
+++ EIT_test_code/plot_voltages.py
@@ -1,17 +1,25 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys 
 
-filename = 'test.txt'
+filename = sys.argv[1] #'actuator_tube.txt'
 
 df = pd.read_csv(filename, sep='\t', lineterminator='\n', skiprows=1, 
 	names=["A", "B", "C", "D"])
 
 #print(df[['B', 'C']])
 
-ax = df[['B', 'C']].plot()
+print(df)
+
+ax = df[['B']].plot()#df[['B', 'C']].plot()
 plt.legend(loc='best')
 
 ax.set_title(filename)
-ax.set_xlabel('Samples')
-ax.set_ylabel('Voltage (mV)')
+ax.set_xlabel('X')
+ax.set_ylabel('Y (mV)')
+
+df[['B']].plot(title=filename, xlabel="X", ylabel="Y (mV)", rot=30)
+
+#plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+
 plt.show()
